87a746c58241e4c7c1d9101c3fe04ff64f604cf9
davjvo <davjvo.dev@gmail.com>
2020-09-10 20:35:46 -0400

    new weather api end point

--- XFWeatherApp/Utils/AppSetting.cs
+++ XFWeatherApp/Utils/AppSetting.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSetting
     {
-        public const string WeatherHost = "community-open-weather-map.p.rapidapi.com";
+        public const string WeatherHost = "https://api.openweathermap.org/";
         public const string WeatherKey = "WEATHER_KEY_HERE";
         public const string MapsHost = "https://maps.googleapis.com/";
         public const string MapsKey = "GMAPS_KEY_HERE";
